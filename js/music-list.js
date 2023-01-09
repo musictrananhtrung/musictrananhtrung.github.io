e13ef964f5a61550d045d37d6ff09dee942e828e
musictrananhtrung <122287934+musictrananhtrung@users.noreply.github.com>
2023-01-09 22:40:17 +0700

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,67 @@
+let allMusic = [{
+        name: "Trời Giấu Trời Mang Đi",
+        artist: "Trần Anh Trung",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Giấc Mơ Rất Thơ",
+        artist: "Trần Anh Trung",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Tình Thật Tình Mơ",
+        artist: "Trần Anh Trung",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Đắp Mộ Magaming",
+        artist: "Trần Anh Trung",
+        img: "2",
+        src: "2"
+    },
+    {
+        name: "Thương Cho Tấm Thân",
+        artist: "Trần Anh Trung",
+        img: "3",
+        src: "3"
+    },
+    {
+        name: "Thà Yêu Lấy Một Người Bình Thường",
+        artist: "Trần Anh Trung",
+        img: "4",
+        src: "4"
+    },
+    {
+        name: "Ngàn Câu Hứa",
+        artist: "Trần Anh Trung",
+        img: "5",
+        src: "5"
+    },
+    {
+        name: "Nắm Tay Nhau",
+        artist: "Trần Anh Trung",
+        img: "6",
+        src: "6"
+    },
+    {
+        name: "Sợ Ngày Mai Em Đi Mất",
+        artist: "Trần Anh Trung",
+        img: "7",
+        src: "7"
+    },
+    {
+        name: "Trót Trao Duyên",
+        artist: "Trần Anh Trung",
+        img: "8",
+        src: "8"
+    },
+    {
+        name: "Ngày Thơ ẤU",
+        artist: "Trần Anh Trung",
+        img: "9",
+        src: "9"
+    },
+];
